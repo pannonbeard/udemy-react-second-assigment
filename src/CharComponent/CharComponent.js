7e9d0764c9576483d8ae29f8a4f90f1c0820c8ac
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-04-05 14:11:28 -0500

    created bare charcomponent

---
+++ src/CharComponent/CharComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const charComponent = () =>{
+  const style = {
+    display: 'inline-block',
+    padding: '16px',
+    textAlign: 'center',
+    margin: '16px',
+    border: '1px solid black'
+  }
+  return(
+    <div style={style}></div>
+  )
+}
+
+export default charComponent
