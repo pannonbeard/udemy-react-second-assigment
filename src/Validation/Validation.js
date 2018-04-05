efc729c84385b37ac2531a21f637dbdc2bd6e906
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-04-05 14:07:02 -0500

    validation checks text

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
-const validation = ({textLength}) =>
-  <p>{textLength}</p>
+const validation = ({textLength}) =>{
+  let valid = null
+  if(textLength < 5){
+    valid = 'Text too short'
+  }else{
+    valid = 'Text long enough'
+  }
+  return(
+    <p>{valid}</p>
+  )
+}
+  
 
 export default validation
