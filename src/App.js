fb7c4f12019667669b126af8f607e8724d0a2743
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-04-05 14:02:17 -0500

    Created input and output text length

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    textLength: 0
+  }
+  changeListener = (e) => {
+    const string = e.target.value
+    const textLength = string.length
+    this.setState({ textLength })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          helo
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.changeListener}/>
+        <p>{this.state.textLength}</p>
       </div>
     );
   }
