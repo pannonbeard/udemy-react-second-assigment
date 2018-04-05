868eec13dce986bd159d6fb521863d5f01c24cd1
Phil Hendricks <phendricks@aea267.k12.ia.us>
2018-04-05 14:03:09 -0500

    Created validation and added to appjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation'
 
 class App extends Component {
   state={
---
+++ src/Validation/Validation.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const validation = ({textLength}) =>
+  <div>{textLeng}</div>
+
+export default validation
